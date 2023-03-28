c4eaae8f1d62929df2557c0dbba08f22a71442c5
Thomas Antony <mail@thomasantony.com>
2023-03-28 22:12:10 +0100

    Add llama_wrapper.cpp and .h

---
+++ src/llama_wrapper.cpp
@@ -0,0 +1,158 @@
+#include "llama_wrapper.h"
+#include <cassert>
+
+static void trigger_cb(float progress, void * user_data) {
+    if (user_data == nullptr) {
+        return;
+    }
+    auto cb = static_cast<Callback*>(user_data);
+    (*cb)(progress);
+}
+
+// Initialize the model
+bool LlamaWrapper::init()
+{
+    if (is_initialized)
+    {
+        return true;
+    }
+    // update pointer to callback if needed
+    if(inference_params.callback)
+    {
+        using raw_cb = void (*)(float, void*);
+        inference_params.ctx_params.progress_callback = (raw_cb)trigger_cb;
+        inference_params.ctx_params.progress_callback_user_data = &inference_params.callback;
+    }else{
+        inference_params.ctx_params.progress_callback = nullptr;
+    }
+    ctx = llama_init_from_file(inference_params.path_model.c_str(), inference_params.ctx_params);
+
+    n_ctx = llama_n_ctx(ctx);
+    last_n_tokens = std::vector<llama_token>(n_ctx);
+    std::fill(last_n_tokens.begin(), last_n_tokens.end(), 0);
+    is_initialized = true;
+    return true;
+}
+// Tokenize text
+const vector<llama_token> LlamaWrapper::tokenize_text(const std::string& text, bool add_bos) const
+{
+    // initialize to prompt numer of chars, since n_tokens <= n_prompt_chars
+    std::vector<llama_token> res(text.size() + (int)add_bos);
+    int n = llama_tokenize(ctx, text.c_str(), res.data(), res.size(), add_bos);
+    assert(n >= 0);
+    res.resize(n);
+    return res;
+}
+
+// Add BOS token to input
+void LlamaWrapper::add_bos() {
+    embd_inp.push_back(llama_token_bos());
+}
+
+// Clear the model input buffer
+void LlamaWrapper::clear_input()
+{
+    embd_inp.clear();
+    n_consumed = 0;
+}
+
+// Set the model input buffer
+void LlamaWrapper::set_input(const std::string& text)
+{
+    set_input(tokenize_text(text));
+}
+
+// Set the model input buffer from tokens
+void LlamaWrapper::set_input(const vector<llama_token>& tokens)
+{
+    embd_inp = tokens;
+    n_consumed = 0;
+}
+
+// Update input with text
+void LlamaWrapper::update_input(const std::string& text)
+{
+    update_input(tokenize_text(text));
+}
+
+// Update input with tokens
+void LlamaWrapper::update_input(const vector<llama_token>& tokens)
+{
+    embd_inp.insert(embd_inp.end(), tokens.begin(), tokens.end());
+}
+
+// Ingest one batch of input
+void LlamaWrapper::ingest_input_batch()
+{
+    // Copy at most n_batch elements from embd_inp to embd
+    size_t num_copied = std::min((size_t) inference_params.n_batch+1, embd_inp.size() - n_consumed);
+    std::copy(embd_inp.begin() + n_consumed,
+                embd_inp.begin() + n_consumed + num_copied,
+                std::back_inserter(embd));
+    n_consumed += num_copied;
+
+    // Copy the last `repeat_last_n` elements copied into embd to last_n_tokens
+    size_t num_copied_last_n = std::min(num_copied, (size_t) inference_params.repeat_last_n);
+    last_n_tokens.erase(last_n_tokens.begin(), last_n_tokens.begin()+num_copied_last_n);
+    last_n_tokens.insert(last_n_tokens.end(), embd.end() - num_copied_last_n, embd.end());
+}
+
+// Ingest all input
+bool LlamaWrapper::ingest_all_pending_input()
+{
+    while (has_unconsumed_input())
+    {
+        ingest_input_batch();
+        eval();
+    }
+    return true;
+}
+
+// Check if there is unconsumed input
+bool LlamaWrapper::has_unconsumed_input() const
+{
+    return n_consumed < embd_inp.size();
+}
+
+// Eval model and clear input
+bool LlamaWrapper::eval()
+{
+    if (embd.size() > 0) {
+        if (llama_eval(ctx, embd.data(), embd.size(), n_past, inference_params.n_threads) != 0) {
+            fprintf(stderr, "Failed to predict\n");
+            return false;
+        }
+    }
+    n_past += embd.size();
+    embd.clear();
+    return true;
+}
+
+// Sample from logits
+llama_token LlamaWrapper::sample()
+{
+    llama_token id = 0;
+
+    {
+        id = llama_sample_top_p_top_k(
+                ctx,
+                last_n_tokens.data() + n_ctx - inference_params.repeat_last_n,
+                inference_params.repeat_last_n,
+                inference_params.top_k,
+                inference_params.top_p,
+                inference_params.temp,
+                inference_params.repeat_penalty
+        );
+
+        last_n_tokens.erase(last_n_tokens.begin());
+        last_n_tokens.push_back(id);
+        embd.push_back(id);
+    }
+    return id;
+}
+
+// Get the logits for the last token
+const float* LlamaWrapper::get_logits() const
+{
+    return llama_get_logits(ctx);
+}
---
+++ src/llama_wrapper.h
@@ -0,0 +1,128 @@
+#ifndef LLAMA_WRAPPER_H
+#define LLAMA_WRAPPER_H
+
+#include "llama.h"
+#include <vector>
+#include <random>
+#include <thread>
+#include <functional>
+
+/* High level wrapper for the C-style LLAMA API */
+using std::vector;
+using Callback = std::function<void(double)>;
+
+struct InferenceParams {
+    // model parameters
+    std::string path_model = "";
+    int32_t seed          = -1;   // RNG seed
+    int32_t n_threads     = std::min(4, (int32_t) std::thread::hardware_concurrency());
+    int32_t n_predict     = 128;  // new tokens to predict
+    int32_t repeat_last_n = 64;   // last n tokens to penalize
+    int32_t n_batch       = 8;    // batch size for prompt processing
+
+    // sampling parameters
+    int32_t top_k = 40;
+    float   top_p = 0.95f;
+    float   temp  = 0.80f;
+    float   repeat_penalty  = 1.10f;
+
+    bool use_mlock = false;
+    bool memory_f16 = false;
+
+    int n_ctx = 512;  // context size
+
+    llama_context_params ctx_params = llama_context_default_params();
+
+    Callback callback{};
+};
+
+class LlamaWrapper {
+    public:
+        // LLAMA API
+        LlamaWrapper() = default;
+        LlamaWrapper(InferenceParams inference_params) 
+            : is_initialized(false), inference_params(inference_params)
+        {}
+        ~LlamaWrapper() {
+            if (ctx)
+            {
+                llama_free(ctx);
+            }
+        };
+
+        // Initialize the model
+        bool init();
+        // Check if the model is initialized
+        bool is_init() const { return is_initialized; }
+
+        // Input processing and inference
+        // Tokenize text
+        const vector<llama_token> tokenize_text(const std::string& text, bool add_bos = false) const;
+        // Queues up a BOS token to the model input
+        void add_bos();
+        // Clears the model input buffer
+        void clear_input();
+        // Set the model input buffer
+        void set_input(const std::string& text);
+        // Set the model input buffer from tokens
+        void set_input(const vector<llama_token>& tokens);
+        // Queues up input text to the model input
+        void update_input(const std::string& text);
+        // Queues up input tokens to the model input
+        void update_input(const vector<llama_token>& tokens);
+        // Ingests input previously added using update_input()
+        void ingest_input_batch();
+        // Ingests all input previously added using update_input() in multiple batches
+        // Batch size is determined by n_batch in InferenceParams
+        bool ingest_all_pending_input();
+        // Checks if the model has unconsumed input to be ingested using ingest_input_batch()
+        bool has_unconsumed_input() const;
+
+        // Evaluate the model on a batch of input. Must call llama_ingest_input_batch() first.
+        bool eval();
+        // Sample token from the model and add it to the model input
+        llama_token sample();
+
+        // Output processing
+        // Get logits
+        const float* get_logits() const;
+
+        // Get embeddings
+        const float* get_embeddings() const {
+            return llama_get_embeddings(ctx);
+        }
+
+        int get_n_vocab() const { return llama_n_vocab(ctx); }
+        int get_n_embd() const { return llama_n_embd(ctx); }
+
+        // Convert token to str
+        std::string token_to_str(llama_token token) const { return llama_token_to_str(ctx, token); }
+
+        // Print timings
+        void print_timings() const { llama_print_timings(ctx); }
+        // Reset timings
+        void reset_timings() const { llama_reset_timings(ctx); }
+
+    private:
+        std::string path_model = "";
+        llama_context* ctx = nullptr;
+        InferenceParams inference_params{};
+
+        // Random number generator
+        std::mt19937 rng{};
+
+        // Tokens
+        vector<llama_token> embd{};
+        vector<llama_token> embd_inp{};
+        vector<llama_token> last_n_tokens{};
+
+        int n_consumed = 0;
+        int remaining_tokens = 0;
+        int n_past = 0;
+        int n_ctx = 0;
+        size_t mem_per_token = 0;
+
+        bool is_initialized = false;
+};
+
+#endif /* LLAMA_WRAPPER_H */
