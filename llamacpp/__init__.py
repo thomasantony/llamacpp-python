c59145aa5d45fdac11f29c3b09e19760af276c4a
Thomas Antony <mail@thomasantony.com>
2023-03-28 22:09:49 +0100

    Move llamacpp to src/ and update __init__.py

--- llamacpp/__init__.py
+++
@@ -1,2 +0,0 @@
-# Expose the bindings in module
-from .llamacpp import gpt_params, PyLLAMA, llama_model_quantize
---
+++ src/llamacpp/__init__.py
@@ -0,0 +1,4 @@
+import llamacpp
+
+# Expose the bindings in module
+from .llamacpp import InferenceParams, LlamaInference, LlamaContext, LlamaContextParams
