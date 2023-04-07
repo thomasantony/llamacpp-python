18917ddc7ad93ed289562fc04197deff9d471e38
Thomas Antony <mail@thomasantony.com>
2023-04-06 20:38:06 -0700

    Update examples to give consistent answers

--- examples/simple.py
+++ examples/simple.py
@@ -3,21 +3,25 @@ import llamacpp
 
 
 def progress_callback(progress):
-    print("Progress: {:.2f}%".format(progress * 100))
-    sys.stdout.flush()
+    # print("Progress: {:.2f}%".format(progress * 100))
+    # sys.stdout.flush()
+    pass
 
 
 params = llamacpp.InferenceParams.default_with_callback(progress_callback)
-params.path_model = './models/7B/ggml-model-q4_0.bin'
+params.path_model = './models/7B/ggml-model-f16.bin'
+params.seed = 19472
+params.repeat_penalty = 1.0
 model = llamacpp.LlamaInference(params)
 
-prompt = "A llama is a"
+prompt = " Llama is"
 prompt_tokens = model.tokenize(prompt, True)
-model.add_bos()
 model.update_input(prompt_tokens)
 
 model.ingest_all_pending_input()
 print(model.system_info())
+
+print(prompt, end='')
 for i in range(20):
     model.eval()
     token = model.sample()
@@ -27,4 +31,4 @@ for i in range(20):
 # Flush stdout
 sys.stdout.flush()
 
-model.print_timings()
+# model.print_timings()
