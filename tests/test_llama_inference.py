8b89832748c4ba1b5c2463e553ed448f2092404b
Thomas Antony <mail@thomasantony.com>
2023-04-06 20:27:04 -0700

    Update test to give consistent results

--- tests/test_llama_inference.py
+++ tests/test_llama_inference.py
@@ -1,16 +1,20 @@
+import os
+import sys
 import pytest
 import llamacpp
 
 
-@pytest.fixture(scope="session")
+# @pytest.fixture(scope="session")
+@pytest.fixture
 def llama_model():
     params = llamacpp.InferenceParams()
-    params.path_model = '../models/7B/ggml-model-f16.bin'
+    # Get path to current file
+    current_file_path = os.path.dirname(os.path.realpath(__file__))
+    # Get path to the model
+    model_path = os.path.join(current_file_path, "../models/7B/ggml-model-f16.bin")
+    params.path_model = model_path
     params.seed = 19472
-    params.top_k = 40
-    params.top_p = 0.95
-    params.repeat_last_n = 64
-    params.n_predict = 8
+    params.repeat_penalty = 1.0
     return llamacpp.LlamaInference(params)
 
 
@@ -35,7 +39,7 @@ def test_token_to_str(llama_model):
 
 
 def test_eval(llama_model):
-    prompt = "Llama is"
+    prompt = " Llama is"
     prompt_tokens = llama_model.tokenize(prompt, True)
     llama_model.update_input(prompt_tokens)
     llama_model.ingest_all_pending_input()
@@ -45,4 +49,8 @@ def test_eval(llama_model):
         token = llama_model.sample()
         output += llama_model.token_to_str(token)
 
-    assert output == " Llama is the newest member of our farm family"
+    assert output == " Llama is the newest member of our growing family"
+
+
+if __name__=='__main__':
+    sys.exit(pytest.main(['-s', '-v', __file__]))
