0585f0fb614b42a84e9873bbf793e0fc370f0e55
Thomas Antony <mail@thomasantony.com>
2023-04-07 20:26:34 -0700

    Add test_eval_exceed_n_ctx and update fixtures

--- tests/test_llama_inference.py
+++ tests/test_llama_inference.py
@@ -4,9 +4,9 @@ import pytest
 import llamacpp
 
 
-# @pytest.fixture(scope="session")
 @pytest.fixture
-def llama_model():
+def nominal_params():
+    """Nominal InfereceParams"""
     params = llamacpp.InferenceParams()
     # Get path to current file
     current_file_path = os.path.dirname(os.path.realpath(__file__))
@@ -15,7 +15,24 @@ def llama_model():
     params.path_model = model_path
     params.seed = 19472
     params.repeat_penalty = 1.0
-    return llamacpp.LlamaInference(params)
+    return params
+
+
+@pytest.fixture
+def limited_context_params(nominal_params):
+    """InferenceParams with limited context"""
+    nominal_params.n_ctx = 32
+    return nominal_params
+
+
+@pytest.fixture
+def llama_model(nominal_params):
+    return llamacpp.LlamaInference(nominal_params)
+
+
+@pytest.fixture
+def limited_context_llama_model(limited_context_params):
+    return llamacpp.LlamaInference(limited_context_params)
 
 
 def test_update_input(llama_model):
@@ -50,6 +67,23 @@ def test_eval(llama_model):
         output += llama_model.token_to_str(token)
 
     assert output == " Llama is the newest member of our growing family"
+
+
+def test_eval_exceed_n_ctx(limited_context_llama_model):
+    # Tests context swapping feature
+    llama_model = limited_context_llama_model
+    prompt = " Llama is"
+    prompt_tokens = llama_model.tokenize(prompt, True)
+    llama_model.update_input(prompt_tokens)
+    llama_model.ingest_all_pending_input()
+    output = prompt
+    # Generate 35 tokens with n_ctx of 32
+    for i in range(35):
+        llama_model.eval()
+        token = llama_model.sample()
+        output += llama_model.token_to_str(token)
+
+    assert output == " Llama is the newest member of our growing family. We’re excited to welcome him to the pack!\nLlama is a male, born in 2017"
 
 
 if __name__=='__main__':
