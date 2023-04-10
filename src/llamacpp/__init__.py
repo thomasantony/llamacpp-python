cbd3935f5ea4d4fc30164f078c7ce2b17b6ada93
Thomas Antony <mail@thomasantony.com>
2023-04-10 14:39:49 -0700

    Fix bug in llamacpp-quantize

--- src/llamacpp/__init__.py
+++ src/llamacpp/__init__.py
@@ -1,4 +1,9 @@
 import llamacpp
 
 # Expose the bindings in module
-from .llamacpp import InferenceParams, LlamaInference, LlamaContext, LlamaContextParams
+from .llamacpp import (InferenceParams,
+    LlamaInference,
+    LlamaContext,
+    LlamaContextParams,
+    llama_model_quantize
+)
